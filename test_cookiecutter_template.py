9c3fd8a2f7e399bda84eb3eb3a91d62e164e7b6a
Joseph Corrado <josephcorrado7@gmail.com>
2025-09-14 06:14:06 -0400

    add build of cookiecutter template to CI/CD

---
+++ test_cookiecutter_template.py
@@ -0,0 +1,23 @@
+def test_bake_project(cookies):
+    result = cookies.bake(
+        extra_context={
+            "project_name": "example_project",
+            "package_name": "example_package",
+            "description": "Example package",
+            "full_name": "John Doe",
+            "email": "john.doe@example.com",
+        }
+    )
+
+    assert result.exit_code == 0
+    assert result.exception is None
+    assert result.project_path.name == "example_project"
+    assert result.project_path.is_dir()
+    assert result.project_path.joinpath("README.md").exists()
+    assert result.project_path.joinpath("pyproject.toml").exists()
+    assert result.project_path.joinpath("tests").exists()
+    assert (
+        result.project_path.joinpath("tests")
+        .joinpath("test_example_package.py")
+        .exists()
+    )

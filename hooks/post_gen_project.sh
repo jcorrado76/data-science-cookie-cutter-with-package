4fcbd35cef3b8097639e15109f7bb23bcfe08864
Joseph Corrado <josephcorrado7@gmail.com>
2025-09-14 06:41:28 -0400

    project creation works now

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-echo "mkdir -p {{ cookiecutter.project_name }}"
-mkdir -p {{ cookiecutter.project_name }}
 echo "uv init --package"
 uv init --package \
-    --directory {{ cookiecutter.project_name }} \
-    --name {{ cookiecutter.package_name }} \
-    --description {{ cookiecutter.description }} \
+    --name '{{ cookiecutter.package_name }}' \
+    --description '{{ cookiecutter.description }}' \
     --author-from git \
     --no-pin-python \
     --managed-python
@@ -24,16 +21,16 @@ cat pyproject-tool-config.toml >> pyproject.toml
 echo "Configuring VSCode to run pytest on tests folder"
 mv .vscode-dev .vscode
 echo "Adding current poetry environment as default Python Interpreter in VSCode"
-path_to_poetry_interpreter=$(poetry run which python)
-echo "Using $path_to_poetry_interpreter as default Python Interpreter"
-awk -v n=$path_to_poetry_interpreter '1;/^{/&&c++==0{print "    \"python.defaultInterpreterPath\": \"" n "\", "} ' .vscode/settings.json > temp && mv temp .vscode/settings.json
-git add tests {{ cookiecutter.package_name }} && poetry run git commit -m "initial commit on {{ cookiecutter.package_name }}"
-git add .circleci && poetry run git commit -m "initial commit on CircleCI Configuration"
-git add .git-dev && git commit -m "initial commit on git configuration files"
-git add .gitignore && git commit -m "add .gitignore"
-git add Makefile && git commit -m "add Makefile"
-git add README.md && git commit -m "add README"
-git add notebooks && git commit -m "add jupyter notebooks folder"
-git add pyproject.toml poetry.lock && git commit -m "add initial dev dependencies"
-git add scripts && git commit -m "add scripts folder with move git hooks script"
-make tests
+path_to_python_interpreter=$(uv run python -c 'import sys; print(sys.executable)')
+echo "Using $path_to_python_interpreter as default Python Interpreter"
+awk -v n=$path_to_python_interpreter '1;/^{/&&c++==0{print "    \"python.defaultInterpreterPath\": \"" n "\", "} ' .vscode/settings.json > temp && mv temp .vscode/settings.json
+# git add tests {{ cookiecutter.package_name }} && poetry run git commit -m "initial commit on {{ cookiecutter.package_name }}"
+# git add .circleci && poetry run git commit -m "initial commit on CircleCI Configuration"
+# git add .git-dev && git commit -m "initial commit on git configuration files"
+# git add .gitignore && git commit -m "add .gitignore"
+# git add Makefile && git commit -m "add Makefile"
+# git add README.md && git commit -m "add README"
+# git add notebooks && git commit -m "add jupyter notebooks folder"
+# git add pyproject.toml poetry.lock && git commit -m "add initial dev dependencies"
+# git add scripts && git commit -m "add scripts folder with move git hooks script"
+# make tests
